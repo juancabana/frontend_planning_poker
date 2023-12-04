45425ee9e648f9ac28dfade41cf3529959e4f341
juancabana <juand.cabanat@unac.edu.co>
2023-12-04 10:18:13 -0500

    f

--- src/app/components/molecules/card/card.component.ts
+++ src/app/components/molecules/card/card.component.ts
@@ -15,5 +15,6 @@ export class CardComponent implements OnInit {
   ngOnInit() {
     this.name_viever = this.player.username.substring(0, 2).toUpperCase();
     this.visualization = this.player.visualization;
+    console.log('d');
   }
 }
