e2124d707a11fd5c891f420438d7c63167d553aa
juancabana <juand.cabanat@unac.edu.co>
2023-12-06 10:49:25 -0500

    First slide done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  animations: [
+    trigger('fadeInOut', [
+
+      transition(':leave', [
+        animate(700, style({ opacity: 0 }))
+      ])
+    ])
+  ]
+
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Planning Poker';
+  showWelcomeMessage = true;
+
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.closeWelcomeMessage();
+    }, 1000);
+
+  }
+  closeWelcomeMessage() {
+    this.showWelcomeMessage = false;
+  }
 }
