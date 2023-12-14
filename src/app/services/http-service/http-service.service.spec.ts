302786f2d3cb5da833638a145545fb6720ee378e
juancabana <juand.cabanat@unac.edu.co>
2023-12-13 23:31:46 -0500

    Http service tested

--- src/app/services/http-service/http-service.service.spec.ts
+++ src/app/services/http-service/http-service.service.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { HttpService } from './http-service.service';
+import { HttpClientModule } from '@angular/common/http';
+import { User } from 'src/app/interfaces/user.interface';
+
 
 describe('HttpServiceService', () => {
   let service: HttpService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule], // proporciona HttpClient
-      providers: [HttpService]
+      imports: [HttpClientModule], // proporciona HttpClient
+      providers: [HttpService],
     });
     service = TestBed.inject(HttpService);
   });
@@ -17,4 +19,87 @@ describe('HttpServiceService', () => {
   test('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+    // Create room
+    test('services - http-service - have to get ', (done) => {
+      service.createNewRoom('Sprint 90').subscribe(room => {
+        expect(room.tittle).toBe('Sprint 90');
+        localStorage.setItem('room_id-1', room._id);
+        done()
+      })
+    });
+
+    test('services - http-service - have to get ', (done) => {
+      service.createNewRoom('Sprint 67').subscribe(room => {
+        expect(room.tittle).toBe('Sprint 67');
+        localStorage.setItem('room_id-2', room._id);
+        done()
+      })
+    });
+
+  // Find room by id
+  test('services - http-service - have to get ', (done) => {
+    const idRoom = localStorage.getItem('room_id-1')!;
+    service.findRoomById(idRoom).subscribe((room) => {
+      expect(room.tittle).toBe('Sprint 90');
+      done();
+    });
+  });
+
+  test('services - http-service - have to get ', (done) => {
+    const idRoom = localStorage.getItem('room_id-2')!;
+    service.findRoomById(idRoom).subscribe((room) => {
+      expect(room.tittle).toBe('Sprint 67');
+      done();
+    });
+  });
+
+  // Create user
+  test('services - http-service - createUser', (done) => {
+    const idRoom = localStorage.getItem('room_id-1')!;
+    const dummyUser: User = { username: 'Juanda', visualization: 'player', room_id: idRoom };
+
+    service.createUser(dummyUser).subscribe(user => {
+      expect(user.username).toEqual(dummyUser.username)
+      done();
+    });
+  });
+
+  test('services - http-service - createUser', (done) => {
+    const idRoom = localStorage.getItem('room_id-1')!;
+    const dummyUser: User = { username: 'Daniel', visualization: 'spectator', room_id: idRoom };
+
+    service.createUser(dummyUser).subscribe(user => {
+      expect(user.username).toEqual(dummyUser.username);
+      done();
+    });
+  });
+
+  // Get players
+  test('services - http-service - getPlayers', (done) => {
+    const idRoom = localStorage.getItem('room_id-1')!;
+
+    service.getPlayers(idRoom).subscribe(players => {
+      expect(players.length).toBe(2);
+      expect(players[0].username).toEqual('Juanda');
+      expect(players[1].username).toEqual('Daniel');
+
+      done();
+    });
+  });
+
+  // Get cards
+  test('services - http-service - getCards', (done) => {
+    service.getCards().subscribe(cards => {
+      expect(cards.length).toBe(12);
+      // expect(cards[0].viewValue).toEqual('0');
+      // expect(cards[3].viewValue).toEqual('5');
+      // expect(cards[5].viewValue).toEqual('13');
+      // expect(cards[11].viewValue).toEqual('☕');
+
+      done();
+    });
+  });
+
+
 });
